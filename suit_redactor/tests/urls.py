e240a594839145b04ed26d06df0dd1a3130e7587
Amos Vryhof <amos@vryhofresearch.com>
2020-02-19 13:43:28 -0500

    Make it so urls.py doesn't fail on tests

--- suit_redactor/tests/urls.py
+++ suit_redactor/tests/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
 admin.autodiscover()
 
 urlpatterns = [
-    path("admin/", include(admin.site.urls)),
+    path("admin/", admin.site.urls, name="admin"),
 ]
